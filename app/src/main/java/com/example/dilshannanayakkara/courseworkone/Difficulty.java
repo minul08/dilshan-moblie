8599a289112cfd5ac23f9a1da818114ffe3c8ce5
Dilshan Nanayakkara <Dilshan Nanayakkara>
2017-03-12 14:56:39 +0530

    updated Difficulty.java a lil bit more

--- app/src/main/java/com/example/dilshannanayakkara/courseworkone/Difficulty.java
+++ app/src/main/java/com/example/dilshannanayakkara/courseworkone/Difficulty.java
@@ -2,6 +2,9 @@ package com.example.dilshannanayakkara.courseworkone;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.ViewFlipper;
 
 /**
  * Created by Dilshan Nanayakkara on 12/03/2017.
@@ -11,9 +14,33 @@ public class Difficulty extends Activity{
 
     public GameData gameData;
 
+    ViewFlipper flipper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView();
+        setContentView(R.layout.difficulty);
+        startNewGame();
+        //switchControl();
+
+    }
+
+    public void startNewGame(){
+
+        Button startNewGame = (Button) findViewById(R.id.NewGameButton);
+
+        final RadioButton difficultyNovice = (RadioButton) findViewById(R.id.radioButtonNovice);
+        gameData = new GameData();
+        if (difficultyNovice.isChecked()){
+            gameData.setdifficultyLevel("novice");
+        }
+
+        final RadioButton difficultyNovice = (RadioButton) findViewById(R.id.radioButtonNovice);
+        final RadioButton difficultyEasy = (RadioButton) findViewById(R.id.radioButtonEasy);
+        final RadioButton difficultyMedium = (RadioButton) findViewById(R.id.radioButtonMedium);
+        final RadioButton difficultyGuru = (RadioButton) findViewById(R.id.radioButtonGuru);
+        //String radioText = "";
+
+
     }
 }
