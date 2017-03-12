47d8eda2a91878a9cb38305265134a50a24bb134
Minul Wimalagunaratne <minul8@live.com>
2017-03-12 14:58:20 +0530

    game play - minul

--- app/src/main/java/com/example/dilshannanayakkara/courseworkone/GamePlay.java
+++ app/src/main/java/com/example/dilshannanayakkara/courseworkone/GamePlay.java
@@ -1,7 +1,10 @@
 package com.example.dilshannanayakkara.courseworkone;
 
+import android.app.Activity;
+
 /**
  * Created by Dilshan Nanayakkara on 12/03/2017.
  */
-public class GamePlay {
+public class GamePlay extends Activity {
+
 }
